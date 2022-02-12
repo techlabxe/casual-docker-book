e6c74d1989ace80c374ae51a49affd902497ea8c
techlabxe <slash@watery.dip.jp>
2022-02-12 16:59:47 +0900

    Redmine Plugin メニューへの追加

--- redmine-3/redmine/data/plugins/polls/init.rb
+++ redmine-3/redmine/data/plugins/polls/init.rb
@@ -3,4 +3,6 @@ Redmine::Plugin.register :polls do
   author '初めてのプラグイン'
   description 'This is a plugin for Redmine'
   version '0.0.1'
+
+  menu :application_menu, :polls, { controller: 'polls', action: 'index'}, caption: 'Polls'
 end
